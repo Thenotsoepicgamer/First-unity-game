8a82005e80d60a1a3a299cadd0027ebecde2974d
Thenotsoepicgamer <zombiesoldier6955yearoldman@gmail.com>
2024-09-17 09:56:46 -0700

    9/17

--- project 1/Assets/scripts/Player Controller.cs
+++ project 1/Assets/scripts/Player Controller.cs
@@ -6,11 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     Vector2 camRotation;
 
+  
+
     Rigidbody myRB;
     Camera playerCam;
+
+    [Header("movement stats")]
+    public bool sprinting = false;
     public float speed = 10f;
     public float jumpHeight = 5f;
 
+    public float grounddetection = 1f;
+
+    [Header("user settings")]
+    public float sprintMult = 1.5f;
+    public bool sprintToggle = false;
     public float mouseSensitivity = 2.0f;
     public float XSensitivity = 2.0f;
     public float YSensitivity = 2.0f;
@@ -55,7 +65,22 @@ public class PlayerController : MonoBehaviour
             temp.x = Input.GetAxisRaw("Horizontal") * speed;
             temp.z = Input.GetAxisRaw("Vertical") * speed;
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (!sprinting && !sprintToggle && Input.GetKey(KeyCode.LeftShift))
+                sprinting = true;
+
+            if (!sprinting && sprintToggle && (Input.GetAxisRaw("vertical") > 0) && Input.GetKey(KeyCode.LeftShift))
+                sprinting = true;
+
+            if (sprinting)
+                temp.z *= sprintMult;
+
+            if (sprinting && sprintToggle && (Input.GetAxisRaw("vertical") <= 0))
+                sprinting = false;
+
+            if (sprinting && !sprintToggle && Input.GetKeyUp(KeyCode.LeftShift))
+                sprinting = false;
+
+            if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position,-transform.up, grounddetection))
                 temp.y = jumpHeight;
 
             myRB.velocity = (transform.forward * temp.z) + (transform.right * temp.x) + (transform.up * temp.y);
